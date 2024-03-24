04d16c39090143a41861154cb7ad64dd7349a1ea
DuncanBiwott <bduncan19@student.kibu.ac.ke>
2024-03-24 23:36:36 +0300

    added login register and home

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,41 +1,27 @@
 <template>
   <div class=" w-100 bg-info d-flex align-items-center justify-content-center m-4 min-vh-100">
-    <form>
+    <form @click.prevent="login">
+      <h1 class="text-center">Login</h1>
+      <div v-if="error" class="alert alert-danger mt-3" role="alert">
+        {{ error }}
+      </div>
+      
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">ID No</label>
-        <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-          v-model="id_number">
-        <div id="emailHelp" class="form-text">We'll never share your information with anyone else.</div>
+        <label for="exampleInputEmail1" class="form-label">Email</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+          v-model="username">
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <input type="password" class="form-control" id="exampleInputPassword1" v-model="password">
       </div>
-      <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      <a href="/forgot">Forgot password</a>
+      <div class="text-end">
+        <button type="submit" class="btn btn-success" @click="login">Login</button>
+
       </div>
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
-          data-bs-toggle="dropdown" aria-expanded="false">
-          Status
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-          <li><a class="dropdown-item" href="#">Active</a></li>
-          <li><a class="dropdown-item" href="#">Inactive</a></li>
-        </ul>
-      </div>
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
-          data-bs-toggle="dropdown" aria-expanded="false">
-          Role
-        </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-          <li><a class="dropdown-item" href="#">Admin</a></li>
-          <li><a class="dropdown-item" href="#">Client</a></li>
-        </ul>
-      </div>
-      <button type="submit" class="btn btn-success" @click="login">Submit</button>
+      
+      <p class="text-center">Don't have an account? <a href="/register/">Register</a></p>
     </form>
   </div>
 </template>
@@ -45,11 +31,14 @@ definePageMeta({
   layout: false
 });
 
-export default {
-  data: {
-    id_number: "",
+export default { 
+  data(){
+    return {
     password: "",
-    success: ""
+    success: "",
+    error: "",
+    username: ""
+    };
 
 
   },
@@ -60,17 +49,18 @@ export default {
         const response = await $fetch('http://127.0.0.1:8000/customers/create/', {
           method: 'POST',
           body: {
-            email: this.email,
+            username: this.username,
             password: this.password
           }
         });
 
-        if (response.status === 200) {
+        if (response.code === '200') {
           this.success = response.message;
         }
 
 
       } catch (e) {
+        this.error = "An error occurred. Please try again later.";
 
       }
     },
